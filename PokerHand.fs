a446b99509089a4e11203f73812b97effafb4c26
Christophe Thibaut <cthibaut@octo.com>
2022-02-25 21:32:10 +0100

    string card rank suit

--- PokerHand.fs
+++ PokerHand.fs
@@ -1,14 +1,44 @@
-
+10 CONSTANT TEN
+11 CONSTANT JACK
+12 CONSTANT QUEEN
+13 CONSTANT KING
 14 CONSTANT ACE
 0 CONSTANT HEARTS
+1 CONSTANT SPADES
+2 CONSTANT DIAMONDS
+3 CONSTANT CLUBS
+
+\ converts a char {0123456789TJQKA} to a rank value
+: CHAR>RANK ( c -- rank )
+    DUP [CHAR] T = IF DROP TEN   ELSE
+    DUP [CHAR] J = IF DROP JACK  ELSE
+    DUP [CHAR] Q = IF DROP QUEEN ELSE
+    DUP [CHAR] K = IF DROP KING  ELSE
+    DUP [CHAR] A = IF DROP ACE   ELSE
+    DUP [CHAR] 0 >= OVER [CHAR] 9 <= AND IF
+        [CHAR] 0 -
+    ELSE
+        DROP S" unknown rank letter" EXCEPTION THROW
+    THEN THEN THEN THEN THEN THEN ;
+
+\ converts a char {HSDC} to a suit value
+: CHAR>SUIT ( c -- suit )
+    DUP [CHAR] H = IF DROP HEARTS ELSE
+    DUP [CHAR] S = IF DROP SPADES ELSE
+    DUP [CHAR] D = IF DROP DIAMONDS ELSE
+    DUP [CHAR] C = IF DROP CLUBS ELSE
+    DROP S" unknown suit letter" EXCEPTION THROW
+    THEN THEN THEN THEN ;
 
 : STRING>CARD ( addr,u -- card )
-    2DROP   \ fake, do nothing with the address and length
-    0       \ fake
-;
+    2 <> IF S" incorrect string length for a card" EXCEPTION THROW
+    ELSE
+        DUP C@  CHAR>RANK    2 LSHIFT
+        SWAP 1+ C@ CHAR>SUIT OR
+    THEN ;
 
 : RANK ( card -- rank )
-    DROP ACE ;  \ fake it
+    2 RSHIFT ;
 
 : SUIT ( card -- suit )
-    DROP HEARTS ;  \ fake it
+    3 AND ;
--- TestPokerHand.fs
+++ TestPokerHand.fs
@@ -3,10 +3,14 @@ REQUIRE PokerHand.fs
 
 : TEST-STRING-CARD-RANK-SUIT
     ." a card value can be created from a string." CR
-    S" AH" STRING>CARD
     ." one can extract the rank and suit from a card value." CR
-    DUP RANK ACE    ?S
-        SUIT HEARTS ?S
+    S" AH" STRING>CARD DUP RANK ACE    ?S SUIT HEARTS ?S
+    S" KH" STRING>CARD DUP RANK KING   ?S SUIT HEARTS ?S
+    S" QS" STRING>CARD DUP RANK QUEEN  ?S SUIT SPADES ?S
+    S" JS" STRING>CARD DUP RANK JACK   ?S SUIT SPADES ?S
+    S" TD" STRING>CARD DUP RANK TEN    ?S SUIT DIAMONDS ?S
+    S" 9C" STRING>CARD DUP RANK 9      ?S SUIT CLUBS   ?S
+    S" 2H" STRING>CARD DUP RANK 2      ?S SUIT HEARTS   ?S
 ;
 
 TEST-STRING-CARD-RANK-SUIT
